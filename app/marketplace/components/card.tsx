f0b9aab1b53e8980036a61f7f8855ad62a184c26
Aaron <1kookyw1@gmail.com>
2024-03-09 21:22:56 -0800

    fixed marketplace modals

--- app/marketplace/components/card.tsx
+++ app/marketplace/components/card.tsx
@@ -29,24 +29,24 @@ const PCard = (data: { key: React.Key | null | undefined; item: string | number 
               <>
                 <ModalHeader className="flex flex-col gap-1">{data.item}</ModalHeader>
                 <ModalBody>
-                    <p>{data.desc}</p>
-                  {/*<p> */}
-                  {/*  Lorem ipsum dolor sit amet, consectetur adipiscing elit.*/}
-                  {/*  Nullam pulvinar risus non risus hendrerit venenatis.*/}
-                  {/*  Pellentesque sit amet hendrerit risus, sed porttitor quam.*/}
-                  {/*</p>*/}
-                  {/*<p>*/}
-                  {/*  Lorem ipsum dolor sit amet, consectetur adipiscing elit.*/}
-                  {/*  Nullam pulvinar risus non risus hendrerit venenatis.*/}
-                  {/*  Pellentesque sit amet hendrerit risus, sed porttitor quam.*/}
-                  {/*</p>*/}
-                  {/*<p>*/}
-                  {/*  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit*/}
-                  {/*  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. */}
-                  {/*  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. */}
-                  {/*  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur */}
-                  {/*  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.*/}
-                  {/*</p>*/}
+                  <div className="flex flex-col gap-1 items-center">
+                    <div className="flex flex-row w-full justify-around pl-10 pr-10">
+                      <Image
+                      shadow="sm"
+                      radius="lg"
+                      width="100%"
+                      // alt={data.title}
+                      className="w-full object-cover h-[140px]"
+                      src={data.img}
+                      />
+                    </div>
+                    <div>
+                      <h1 className="font-bold">Price:</h1>
+                      <h1>{data.price}</h1>
+                      <h1 className="font-bold">Description:</h1>
+                      <p>{data.desc}</p>
+                    </div>         
+                  </div>
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={onClose}>
