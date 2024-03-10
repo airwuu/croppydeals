6dcadf85ebcdbaa5d1455bb08de07c3809509c2e
Ryan Mar <69983436+seika5@users.noreply.github.com>
2024-03-09 17:37:08 -0800

    implemented firebase data into marketplace

--- app/marketplace/components/produceCards.tsx
+++ app/marketplace/components/produceCards.tsx
@@ -1,59 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, DocumentData } from 'firebase/firestore';
+import { firestore } from '../../../firebase/firebase';
 import PCard from './card';
 
-const ProduceCards = () => {
-    const list = [
-        {
-          title: "Orange",
-          img: "/images",
-          price: "$5.50",
-        },
-        {
-          title: "Tangerine",
-          img: "/images",
-          price: "$3.00",
-        },
-        {
-          title: "Raspberry",
-          img: "/images",
-          price: "$10.00",
-        },
-        {
-          title: "Lemon",
-          img: "/images",
-          price: "$5.30",
-        },
-        {
-          title: "Avocado",
-          img: "/images",
-          price: "$15.70",
-        },
-        {
-          title: "Lemon 2",
-          img: "/images",
-          price: "$8.00",
-        },
-        {
-          title: "Banana",
-          img: "/images",
-          price: "$7.50",
-        },
-        {
-          title: "Watermelon",
-          img: "/images",
-          price: "$12.20",
-        },
-    ];
-  return (
-    <div className='flex flex-row justify-center mx-3 pl-5'>
-        <div  className="cards flex flex-row justify-start flex-wrap gap-5 max-w-7xl pt-9">
-            {list.map((item, index) => (
-                <PCard title={item.title} img={item.img} price={item.price} key={index}></PCard>
-            ))}
-        </div>
-    </div>
-    
-  )
+interface ProduceItem {
+  title: string;
+  img: string;
+  price: string;
 }
 
-export default ProduceCards
+const ProduceCards = () => {
+  const [produceList, setProduceList] = useState<ProduceItem[]>([]);
+
+  useEffect(() => {
+    const fetchProduceData = async () => {
+      const querySnapshot = await getDocs(collection(firestore, 'produce'));
+      const data = querySnapshot.docs.map(doc => doc.data() as ProduceItem);
+      setProduceList(data);
+    };
+  }, []);
+
+  return (
+    <div className='flex flex-row justify-center mx-3 pl-5'>
+      <div className="cards flex flex-row justify-start flex-wrap gap-5 max-w-7xl pt-9">
+        {produceList.map((item, index) => (
+          <PCard
+            title={item.item}
+            img={item.img}
+            price={item.price}
+            key={index}
+          ></PCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProduceCards;
--- app/marketplace/data.tsx
+++ app/marketplace/data.tsx
@@ -1,11 +1,36 @@
-export const produce = [
-  {label: "Strawberries", value: "strawberries", description: "a"},
-  {label: "Corn", value: "corn", description: "a"},
-  {label: "Blueberries", value: "blueberries", description: "a"},
-  {label: "Potatoes", value: "potatoes", description: "a"},
-  {label: "Tomatoes", value: "tomatoes", description: "a"},
-  {label: "Grass", value: "grass", description: "a"},
-];
+import { firestore } from "../../firebase/firebase";
+import { getDocs, collection } from "firebase/firestore";
+
+
+export interface ProduceItem {
+    item: string;
+    item2: string;
+    desc: string;
+}
+
+const getData = async (): Promise<ProduceItem[]> => {
+    const ref = collection(firestore, 'produce');
+    const snap = await getDocs(ref);
+    const produce: ProduceItem[] = [];
+
+    snap.forEach((doc) => {
+        const { item, item2, desc } = doc.data();
+        produce.push({ item, item2, desc });
+    });
+
+    return produce;
+};
+
+export const produce = getData();
+
+// export const produce = [
+//   {label: "Strawberries", value: "strawberries", description: "a"},
+//   {label: "Corn", value: "corn", description: "a"},
+//   {label: "Blueberries", value: "blueberries", description: "a"},
+//   {label: "Potatoes", value: "potatoes", description: "a"},
+//   {label: "Tomatoes", value: "tomatoes", description: "a"},
+//   {label: "Grass", value: "grass", description: "a"},
+// ];
 
 export const categories = [
     {label: "Fruits", value: "fruits", description: "a"},
